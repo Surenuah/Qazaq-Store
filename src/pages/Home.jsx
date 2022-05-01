9da5d654ff5688c32b8449460f92dff849cdf85e
ANUAR_ARSTANBEKOV <kaliroseswiththegoldengun@gmail.com>
2022-05-01 17:23:46 +0600

    Adding Home.jsx page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Announcements from '../components/Announcements'
+import Categories from '../components/Categories'
+import Navbar from '../components/Navbar'
+import Slider from '../components/Slider'
+
+export default function () {
+    return (
+        <div>
+            <Announcements/>
+            <Navbar/>
+            <Slider/>
+            <Categories/>
+        </div>
+    )
+}
