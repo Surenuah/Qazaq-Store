e4b7a69ecd654e3fca2c962eeb3dbb4dd97fccfd
ANUAR_ARSTANBEKOV <kaliroseswiththegoldengun@gmail.com>
2022-05-01 17:28:55 +0600

    Adding CategoriesItems.jsx component

---
+++ src/components/CategoriesItems/CategoriesItems.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+  position: relative;
+
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`
+const Info = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+`
+const Title = styled.h1`
+  c
+`
+const Button = styled.button``
+
+
+export const CategoriesItems = ({item}) => {
+    return (
+        <Container>
+            <Image src={item.img}/>
+            <Info>
+                <Title>{item.title}</Title>
+                <Button>SHOP NOW</Button>
+            </Info>
+        </Container>
+    )
+}
