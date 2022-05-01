7fcd019ba81ade162ef4c5b3641c7a1397d252c6
ANUAR_ARSTANBEKOV <kaliroseswiththegoldengun@gmail.com>
2022-05-01 17:14:58 +0600

    Correcting files and adding some data(data.js)

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://www.theblistblog.com/wp-content/uploads/2018/10/fashion-friends-friendship-1345082.jpg",
+        title: "SALE 40%",
+        description: "DO NOT MISS THIS OFFER",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://a.lmcdn.ru/pi/bs894x561/1/97/m_1788x1122.jpg",
+        title: "SALE 40%",
+        description: "DO NOT MISS THIS OFFER",
+        bg: "f8e9fd"
+    },
+    {
+        id: 3,
+        img: "../Images/woman's_clothes.jpg",
+        title: "SALE 40%",
+        description: "DO NOT MISS THIS OFFER",
+        bg: "d3ffce"
+    },
+]
+
+export const categories = [{
+    id: 1,
+    img: "https://o-tendencii.com/uploads/posts/2022-02/1644810379_25-o-tendencii-com-p-obraz-dorogaya-odezhda-foto-27.jpg",
+    title: "SALE 40%",
+    description: "DO NOT MISS THIS OFFER",
+    bg: "f5fafd"
+},
+    {
+        id: 2,
+        img: "https://saks.kz/uploads/user_8/2018_02_11__03_49_20__798.jpg",
+        title: "SALE 40%",
+        description: "DO NOT MISS THIS OFFER",
+        bg: "ffd700"
+    },
+    {
+        id: 3,
+        img: "https://darshoes.ru/800/600/https/fashiioncarpet.com/wp-content/uploads/2017/10/IMG_7832.jpg",
+        title: "SALE 40%",
+        description: "DO NOT MISS THIS OFFER",
+        bg: "d3ffce"
+    },
+]
